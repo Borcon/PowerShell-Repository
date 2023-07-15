936f37d1d096996d5a8046f797b999ae64c5080a
Borcon <39000945+Borcon@users.noreply.github.com>
2023-07-15 23:39:07 +0200

    Update Test.ps1

--- Test/Test.ps1
+++ Test/Test.ps1
@@ -1,3 +1,26 @@
+param(
+     [Parameter()]
+     [string]$Parameter1,
+
+     [Parameter()]
+     [string]$Parameter2,
+
+     [Parameter()]
+     [switch]$SwitchParam1,
+
+     [Parameter()]
+     [switch]$SwitchParam2
+ )
+
 Write-Host "Hello World" -ForegroundColor Cyan
 Write-Host "This is a remote Execution Test" -ForegroundColor Cyan
+
+if($Parameter1 -eq "Param1") {
+  Write-Host "Parameter 1 found"
+}
+
+if($Parameter2 -eq "Param2") {
+  Write-Host "Parameter 2 found"
+}
+
 pause "Press any key to continue"
